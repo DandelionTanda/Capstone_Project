3b7b718e9044efb859c8901fd895d0e9326148dd
HH <H29112000@gmail.com>
2021-07-31 10:23:33 +0700

    get info user and display in 'me' screen

--- src/Discount.js
+++ src/Discount.js
@@ -91,18 +91,46 @@ const Home = ( {navigation} ) => {
 
 function Me( { navigation } ) {
 
+  //get token
+  const token =  localStorage.getItem('token')
+  //get type of token
+  const tokenType =  localStorage.getItem('tokenType')
+  //data header
+  const config = {
+    method: "GET",
+    headers: {Authorization: localStorage.getItem('tokenType')+ ' ' +localStorage.getItem('token')}};
+  const [userName, setUserName] = useState('')
+  const [photo, setPhoto] = useState('')
   function clear(){
     navigation.navigate('Login')
     AsyncStorage.clear()
-    
+    localStorage.clear()
   }
+  const url = `https://my.tanda.co/api/v2/users/me`
+  fetch(url,config)
+  .then(res=>res.json())
+   .then((res)=>{
+    try{
+      setUserName(res.name)
+      setPhoto(res.photo)
+    }
+    catch(err)
+    {
+      console.log("err")
+    }
+   });
+  
   return (
     <View style={{ flex: 1, justifyContent: 'center',  width:'50%', alignSelf:'center' }}>
-      
+      <Image style={{ width: '100%', height: '40%', justifyContent: 'center',alignItems: 'center', marginBottom: 30 }} 
+      source={{
+          uri: photo,
+       }} alt = "Avatar"></Image>
+      <Text style={{fontSize: 35, fontWeight: "bold", color: 'black',marginLeft:40,marginBottom:40}}>Welcome {userName}</Text>
       <TouchableOpacity
         style={styles.logoutButton}
         onPress={clear}
-      > 
+      >
         <Ionicons name="log-out" size={35} color="white" />     
         <Text style={{fontSize: 20, fontWeight: "bold", color: 'white',marginLeft:40}}>Log out</Text>
       </TouchableOpacity>
