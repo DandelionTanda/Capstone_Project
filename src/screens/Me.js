2be648022f4e6bec05ac3dc60bc628319821d59d
HH <H29112000@gmail.com>
2021-08-13 18:49:06 +0700

    update UI me screen

--- src/screens/Me.js
+++ src/screens/Me.js
@@ -1,6 +1,6 @@
 import { useState }  from "react";
 import * as React from 'react';
-import { Button, FlatList, SafeAreaView, StatusBar, StyleSheet, Text, TouchableOpacity,View,Icon,Image } from "react-native";
+import { Button, FlatList, SafeAreaView, StatusBar, StyleSheet, Text, TouchableOpacity,View,Icon,Image, ScrollView } from "react-native";
 import 'react-native-gesture-handler';
 import { NavigationContainer, getFocusedRouteNameFromRoute, } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
@@ -15,31 +15,45 @@ export default function Me( { navigation } ) {
   }
   
   return (
-    <View style={{ flex: 1,  width:'70%', alignSelf:'center' }}>
-      <View style={{flexDirection: "row", marginTop:40,alignSelf:'center'}}>
+    <ScrollView style={{ flex: 1, alignSelf:'center' }}>
+      {/* View for image */}
+      <View style={{flexDirection: "row", marginTop:20,alignSelf:'center'}}>
       {localStorage.getItem('photo')!=='null'?
-      <Image style={{ width: 100, height: 100, borderRadius: 100/ 2, marginBottom: 30 }} 
+      <Image style={{ width: 150, height: 150, borderRadius: 130/2 }} 
       source={{
           uri: localStorage.getItem('photo'),
        }} alt = "Avatar"></Image>:
        <MaterialCommunityIcons name="account-circle"  size={100}/>}
-      <Text style={{ fontSize: 35, fontWeight: "bold", color: 'black',marginLeft:40,marginTop:30}}>{localStorage.getItem('name')}</Text>
       </View>
+      {/* View for information: Name, Email, ID, Role, Company*/}
+      <View style= {{alignSelf : 'auto', marginTop: 30}}>
+      <Text style={styles.label}>Name:</Text>
+      <Text style={styles.infor}>{localStorage.getItem('name')}</Text>
+      <Text style={styles.label}>Email:</Text>
+      <Text style={styles.infor}>{localStorage.getItem('email')}</Text>
+      <Text style={styles.label}>Company:</Text>
+      <Text style={styles.infor}>{localStorage.getItem('name')}</Text>
+      <Text style={styles.label}>Role:</Text>
+      <Text style={styles.infor}>{localStorage.getItem('permissions')}</Text>
+      <Text style={styles.label}>ID:</Text>
+      <Text style={styles.infor}>{localStorage.getItem('id')}</Text>
+      </View>
+      {/* logout button */}
       <TouchableOpacity
         style={styles.logoutButton}
         onPress={clear}
-
       >
         <Ionicons name="log-out" size={35} color="white"/>
-        <Text style={{fontSize: 20, fontWeight: "bold", color: 'white',marginLeft:70}}>Log out</Text>
+        <Text style={{fontSize: 20, fontWeight: "bold", color: 'white'}}>Log out</Text>
       </TouchableOpacity>
-    </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   logoutButton: {
-    marginTop: 200,
+    alignSelf:"center",
+    marginTop: 50,
     alignItems: "center",
     backgroundColor: "#E3310E",
     padding: 10,   
@@ -48,5 +62,19 @@ const styles = StyleSheet.create({
     borderTopRightRadius: 15,
     borderBottomLeftRadius: 15,
     borderBottomRightRadius: 15,
+    marginBottom:40
+  },
+  label:{
+    marginLeft:24,
+    marginTop:10,
+    fontSize: 30,
+    fontWeight: "bold",
+    color: 'black'
+  },
+  infor:{
+    marginLeft:50,
+    marginRight:30,
+    fontSize: 22,
+    color: '#232A22'
   },
 });
